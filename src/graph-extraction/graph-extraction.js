6e4df97ff0fb504d32264ade986cbb866e164065
Tom <lorentom@b-tu.de>
2023-04-27 20:43:44 +0200

    added latex & graph path generation

--- src/graph-extraction/graph-extraction.js
+++ src/graph-extraction/graph-extraction.js
@@ -1,4 +1,6 @@
 const { buildSchema, parse, printSchema } = require('graphql');
+const { graphql } = require("graphql");
+const util = require('util')
 
 const typeDefs = `
 type Book {
@@ -83,6 +85,66 @@ paths.forEach((path) => {
     console.log(path.join(" -> "));
 });
 
+function getInputVariables(path) {
+    const objectTypeDefinition = ast.definitions.find(
+        (definition) =>
+            definition.kind === "ObjectTypeDefinition" &&
+            definition.name.value === path[0]
+    );
+
+    if (objectTypeDefinition) {
+        const fieldDefinition = objectTypeDefinition.fields.find(
+            (field) => field.name.value === path[1]
+        );
+
+        if (fieldDefinition && fieldDefinition.arguments.length > 0) {
+            return fieldDefinition.arguments;
+        }
+    }
+
+    return [];
+}
+
+
+
+function generateDummyValue(type) {
+    switch (type) {
+        case "Int":
+            return 1;
+        case "Float":
+            return 1.0;
+        case "String":
+            return "dummy";
+        case "Boolean":
+            return true;
+        case "ID":
+            return "1";
+        default:
+            return null;
+    }
+}
+
+const testQuery = (path) => {
+    const fieldsPath = path.slice(1); // Remove the type name
+    const inputVariables = getInputVariables(path);
+    console.log(inputVariables);
+    const inputValues = inputVariables
+        .map((arg) => `${arg.name.value}: ${generateDummyValue(arg.type.name)}`)
+        .join(", ");
+
+    const query = `
+    query {
+      ${fieldsPath.join("{ ")}${inputValues ? `(${inputValues})` : ""}${"}".repeat(fieldsPath.length)}
+    }
+  `;
+    console.log(query);
+    return graphql(schema, query);
+};
+
+paths.forEach((path, index) => {
+        const result = testQuery(path);
+        console.log(result);
+});
 
 
 
