08703a0cea0fc2778ea0c2a770b86a47d6525065
Tom Lorenz <lorentom@b-tu.de>
2023-05-12 16:57:19 +0200

    added empty clojure project

---
+++ src/graphql-tester/src/graphql_tester/core.clj
@@ -0,0 +1,7 @@
+(ns graphql-tester.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ src/graphql-tester/test/graphql_tester/core_test.clj
@@ -0,0 +1,7 @@
+(ns graphql-tester.core-test
+  (:require [clojure.test :refer :all]
+            [graphql-tester.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
